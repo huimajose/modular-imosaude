cc54cb5d6c0ee92bf27030f6d566996670f9ab51
Huima Jose <imoicalda@gmail.com>
2024-06-10 11:14:08 +0100

    Initiated config.py file in server1 folder

---
+++ server1/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SSL_CERT_PATH = 'certs/cert.pem'
+    SSL_KEY_PATH = 'certs/key.pem'
+    FILE_SERVER_URL  = 'https://localhost:5001/get_file'
+    FILE_SERVER_CERT_PATH = 'certs/cert.pem'
